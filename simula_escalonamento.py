764ffa5f4971865c9729be783fbd51e9c03aae83
lanerson <lanerson153@gmail.com>
2024-11-07 16:23:40 -0300

    com erros

--- simula_escalonamento.py
+++ simula_escalonamento.py
@@ -16,10 +16,10 @@ def situacaoFilas():
     return [len(fila_0), len(fila_1), len(fila_flfs), len(fila_es)]
 
 def menuTeste():    
-    # burst = [8,40,10,30]
-    # es= [3,1,2,1]
-    burst = [50,20]
-    es= [1,2]
+    burst = [8,40,10,30]
+    es= [3,1,2,1]
+    # burst = [50,20]
+    # es= [1,2]
     for i in range(len(burst)):
         novo_processo = []
         for j in range(es[i]):
@@ -173,17 +173,7 @@ def execucao():
         T+=1
         s_fim = situacaoFilas()
         fila_na_hora = s_fim
-        if s_inicio[3] > s_fim[3] and (s_fim[1]+s_fim[2] > 0):             
-            if s_fim[0] == 1:
-                tf0+=1                                 
-            if(s_fim[1]>0 and s_fim[0] == 1):
-                gantt += f" {fila_1[0][0]} {T}"                 
-                fila_1[0][1][0]-=1 
-                fila_0[0][1][0]+=1
-            elif(s_fim[2]>0 and s_fim[0] == 1):
-                gantt += f" {fila_flfs[0][0]} {T}"
-                fila_flfs[0][1][0]-=1  
-                fila_0[0][1][0]+=1                           
+                                  
     print(gantt)
         
         
